2382d82c04ed92a60422657de4c4e96db6b4dbfa
codekarey <codekarey@gmail.com>
2020-02-14 16:22:19 -0500

    Basic program outline with ToDoList class

---
+++ ToDoID/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ToDoID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //display sample task list 
+
+
+            bool yes = true;
+            while (yes)
+            {
+                //options
+                Console.WriteLine("[ 1 ] List Tasks\n[ 2 ] Add Task\n[ 3 ] Delete Task\n" +
+                                  "[ 4 ] Mark Task Complete\n[ 5 ] Quit");
+                //check input is valid
+                int navNum = TryCatchInput("");
+
+                //nav options
+                switch (navNum)
+                {
+                    //list all(if null use sample)
+                    case 1:
+
+                        break;
+                    //add new task
+                    case 2:
+
+                        break;
+                    //delete task(IF owner)
+                    case 3:
+
+                        break;
+                    //mark complete
+                    case 4:
+
+                        break;
+                    //exit loop
+                    case 5:
+                        Console.WriteLine("Okay, Goodbye.");
+                        Console.ReadLine();
+                        yes = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Sorry, thats not an option.");
+                        yes = true;
+                        break;
+                }
+
+            }
+
+        }
+        //checks input is valid & provides error message
+        public static int TryCatchInput(string message)
+        {
+            string input = ToDoList.Get(message);
+            try
+            {
+                int userInput = int.Parse(input);
+                return userInput;
+            }
+            catch (FormatException)
+            {
+                return TryCatchInput("That was not a number. Please enter a number.");
+            }
+        }
+    }
+}
---
+++ ToDoID/ToDoList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoID
+{
+    class ToDoList
+    {
+
+
+        //props
+        public string TaskName { get; set; }
+        public string TaskFor { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string DueDate { get; set; }
+        public int Count { get; set; }
+
+        //construct
+        public ToDoList()
+        {
+
+        }
+
+        public ToDoList(string taskname, string taskfor, string description, string status, string duedate)
+        {
+            TaskName = taskname;
+            TaskFor = taskfor;
+            Description = description;
+            Status = status;
+            DueDate = duedate;
+        }
+
+        public void ShowList()
+        {
+            {   //output format
+                Console.WriteLine("_________________________________________________");
+                Console.WriteLine("Task Number " + (Count) + "]  Team member: " + TaskFor + "");
+                Console.WriteLine("Task: "+TaskName+"   Description: " + Description);
+                Console.WriteLine("Due Date: " + DueDate + "   Status: " + Status);
+                Console.WriteLine("_________________________________________________");
+            }
+        }
+
+        public static List<ToDoList> DoList(List<ToDoList> list)
+        {
+            foreach(ToDoList toDo in list)
+            {
+                toDo.ShowList();
+            }
+            return list;
+        }
+
+        //sets value to user input
+        public static string Get(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string inputPrompted = Console.ReadLine();
+            return inputPrompted;
+        }
+
+        //lets the user edit the status of the task selected
+        public static List<ToDoList> StatusIs(string input, int number, List<ToDoList> statusUpdate)
+        {
+            //if y: user wants to mark complete
+            if (input.ToLower() == "yes" || input.ToLower() == "y")
+            {
+                //find the task # the user wants to change to get status index
+                statusUpdate[number - 1].Status = "[X] Complete";
+                return statusUpdate;
+            }
+            //if n: user wants to mark not complete
+            if (input.ToLower() == "no" || input.ToLower() == "n")
+            {
+                statusUpdate[number - 1].Status = "[ ] Not Complete";
+                return statusUpdate;
+            }
+            return statusUpdate;
+        }
+    }
+}
